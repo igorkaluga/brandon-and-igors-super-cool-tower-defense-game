4d1b27af765cef4af4d6291dd3c6af51a486e216
Igor <igorkaluga00@gmail.com>
2022-12-14 20:41:02 -0700

    refactored code for matrix rotation and added path creation functionality

--- make_rand_level.gd
+++ make_rand_level.gd
@@ -3,42 +3,12 @@ extends EditorScript
 
 var height
 var width
-var map_matrix = []
 var matrix_row
-var rng = RandomNumberGenerator.new()
-
-# FILLED MATRIX VER (1)
-func make_matrix(height, width):
-	"""
-		Generates height x width matracie of ones and zeros.
-		1 - taken tile
-		2 - empty tile
-	"""
-	for i in height:
-		matrix_row = []
-		var one_counter = 0
-		
-		for j in width:
-			var tile = round(randf())
-			
-			#tile count logic
-			if tile == 1:
-				one_counter += 1
-			if one_counter > 5:
-				matrix_row.append(0)
-				
-			else:
-				matrix_row.append(tile)
-				
-		map_matrix.append(matrix_row)
-
-var Tile = {
-	0: "Empty",
-	1: "Full"
-}
+var random = RandomNumberGenerator.new()
 
 # EMPTY MATRIX VER (1)
 var empty_matrix = []
+
 func make_empty_matrix(height, width):
 	for i in height:
 		matrix_row = []
@@ -47,69 +17,84 @@ func make_empty_matrix(height, width):
 		empty_matrix.append(matrix_row)
 		
 func get_direction():
-	# [x,y]
+	# [x,y] coordinates
 	# [up, right, left, down]
 	var direction_arr = [[0,1], [1,0], [-1,0], [0,-1]]
 	var direction = direction_arr[randi()%4]
+	
 	return direction
 
 func reflect_matrix_move(x, y, matrix_len):
+	"""
+		Maps the directional move from orignal matrix to the flipped matrix.
+	"""
+	var cur_x = matrix_len - y - 1
+	var cur_y = x
 	
+	return [cur_x, cur_y]
 	
-	pass
+func mark_tile(tile_loc, matrix, marker):
+	var x_cord = tile_loc[0]
+	var y_cord = tile_loc[1]
+	
+	matrix[x_cord][y_cord] = marker
 
 func make_path(matrix, start_tile):
 	""" 
-		Runs on empty matrix
-		v.1
+		Creatres a path in an empty matrix.
+		start_tile is the starting point for the enemy.
+		Maintain matrices:
+			- Original (for tracking directional changes)
+			- Flipped (to render the map)
 	"""
-	# marks starting point on "flipped matrix"
-	var cur_x = len(matrix) - start_tile[1] - 1
-	var cur_y = start_tile[0]
-	matrix[cur_x][cur_y] = "M"
+	var tile_cnt = 1 # for logic later on
 	
-	# track moves on original tiles
-	var pre_cur_x = start_tile[0]
-	var pre_cur_y = start_tile[1]
+	# on creation mark the entrance tile as "A".
+	var flipped_arr = reflect_matrix_move(start_tile[0], start_tile[1], len(matrix))
+	mark_tile(flipped_arr, matrix, "A")
 	
-	# gets the direction of the next move
-	var cur_direction = [0,-1] #get_direction()
+	# maintains unflipped matrix
+	var original_tile = start_tile
 	
-	# make move on original matrix
-	pre_cur_x += cur_direction[0]
-	pre_cur_y += cur_direction[1]
+	var gen_map = true
+	var gen_map_cnt = 0
 	
-	# reflect move on "flipped matrix"
-	cur_x = len(matrix) - pre_cur_y - 1
-	cur_y = pre_cur_x
-	
-	# modifies matrix 
-	matrix[cur_x][cur_y] = "X"
-	
-	"""
-	for i in range(0,1):
-		var cur_direction = get_direction()
-		cur_x += cur_direction[0]
-		cur_y += cur_direction[1]
-	"""
+	while gen_map_cnt < 2:
 		
+		var path_direction = get_direction()
+		
+		print(path_direction)
+		
+		random.randomize()
+		var path_length = random.randi_range(3,6)
+		
+		print(path_length)
+	
+		for i in range(0, path_length):
+				
+			var tile_marker = "X"
+			
+			# make move on original matrix
+			original_tile[0] += path_direction[0]
+			original_tile[1] += path_direction[1]
+			
+			# reflect move on "flipped matrix"
+			flipped_arr = reflect_matrix_move(original_tile[0], original_tile[1], len(matrix))
+			mark_tile(flipped_arr, matrix, tile_marker)
+			
+			tile_cnt += 1
+		
+		gen_map_cnt += 1
 	
 func _run():
-	make_matrix(5,5)
-	make_empty_matrix(5,5)
-	#make_path(map_matrix)
-	
+	make_empty_matrix(10,10)
+
 	make_path(empty_matrix, [0,3])
+	
+	#prints matrix for visualization
+	print()
 	for row in empty_matrix:
 		print(row)
-
-
-
-
-
-
-
-
 
 
 
